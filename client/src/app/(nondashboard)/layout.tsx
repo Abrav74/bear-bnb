db34cb42722b1f66cfdcde1305adf83b85e51bd3
Abrav74 <abrav74@gmail.com>
2025-09-17 23:16:57 -0700

    made some changes to config and the navbar

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,11 +1,11 @@
+import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      {/* <Navbar /> */}
-      NAVBAR
+      <Navbar />
       <main className={`h-full w-full flex flex-col pt-[${NAVBAR_HEIGHT}px]`}>
         {children}
       </main>
