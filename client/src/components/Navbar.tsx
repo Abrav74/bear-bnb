dc1974fa8fa83cbca3ebc7ecaed2fcfb4f802333
Abrav74 <abrav74@gmail.com>
2025-09-18 23:56:26 -0700

    added button functionality to nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { NAVBAR_HEIGHT } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
       className="fixed top-0 left-0 w-full z-50 shadow-xl"
       style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
-      <div className="flex justify-between item-center w-full py-3 px-8 bg-primary-700 text-white">
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
         <div className="flex items-center gap-4 md:gap-6">
           <Link
             href="/"
@@ -36,6 +37,24 @@ const Navbar = () => {
         <p className=" text-primary-200 hidden md:block">
           Find your perfect showcase space with our advanced search
         </p>
+        <div className="flex items-center gap-5">
+          <Link href="/signin">
+            <Button
+              variant="outline"
+              className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
+            >
+              Sign In
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button
+              variant="outline"
+              className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
+            >
+              Sign Up
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
