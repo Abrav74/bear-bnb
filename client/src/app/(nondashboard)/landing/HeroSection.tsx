44707f3d17fb9ae4683bafb14a0a7b8fc2a12e47
Abrav74 <abrav74@gmail.com>
2025-09-20 23:25:57 -0700

    adding in hero img/specs

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="relative h-screen">
+      <Image
+        src="/landing-splash.jpg"
+        alt="Commmunity Space Rental Platform Hero Section"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+    </div>
+  );
 };
 
 export default HeroSection;
