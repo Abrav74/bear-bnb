b9716e18b78845fb62ea097078ccc8116a90e7e7
Abrav74 <abrav74@gmail.com>
2025-09-23 20:31:25 -0700

    stting up key animations for feature section

--- client/src/app/(nondashboard)/landing/FeaturesSection.tsx
+++ client/src/app/(nondashboard)/landing/FeaturesSection.tsx
@@ -1,4 +1,31 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const FeaturesSection = () => {
   return <div>FeaturesSection</div>;
