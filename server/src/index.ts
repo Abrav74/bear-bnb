329bf9b8b00c8519649eacba5e5399393c43ac54
Abrav74 <abrav74@gmail.com>
2025-11-18 20:23:58 -0800

    completed baseline setup for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,4 +5,26 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 
-// ROUTE IMOPRTS
+// ROUTE IMPORTS
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// SERVER
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
