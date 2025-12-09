ff1becc75fe64cfc372159958b43a3efae993f51
Abrav74 <abrav74@gmail.com>
2025-12-08 19:50:14 -0800

    configured Amplify

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -4,7 +4,15 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure();
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID!,
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_CLIENT_ID!,
+    },
+  },
+});
 
 export default function App() {
   return (
